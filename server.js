ece497c8aa5a9f0d9a1bccbbc5a7ab8aa053aee4
knavekaiser <naeem.ahmad.9m@gmail.com>
2020-09-19 11:50:43 +0600

    PORT

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const DB = require("nedb");
 
-const Port = process.env.Port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const database = new DB("database.db");
 database.loadDatabase();
 
-app.listen(Port, () => console.log("server just ran"));
+app.listen(PORT, () => console.log("server just ran"));
 app.use(express.static("public"));
 app.use(express.json());
 
