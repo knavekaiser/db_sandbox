9239b72f267d3cb286cfaffddfc3cdca4d997299
knavekaiser <naeem.ahmad.9m@gmail.com>
2020-09-19 16:18:02 +0600

    mongodb

--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
 const $ = (selector) => document.querySelector(selector);
 
-const list = $("#list");
-$("form").addEventListener("submit", (e) => {
+const list = $("#nedbList");
+$("#nedb").addEventListener("submit", (e) => {
   e.preventDefault();
   const data = {
-    name: $("form #name").value,
+    name: $("#nedb #name").value,
   };
   const options = {
     method: "POST",
@@ -14,9 +14,35 @@ $("form").addEventListener("submit", (e) => {
     body: JSON.stringify(data),
   };
   e.target.reset();
-  fetch("/api", options)
+  fetch("/nedb", options)
     .then((res) => res.json())
     .then((data) => {
+      console.log("nedb", data);
+      list.innerHTML = "";
+      data.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item.name;
+        list.appendChild(li);
+      });
+    });
+});
+$("#mongo").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = {
+    name: $("#mongo #name").value,
+  };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  e.target.reset();
+  fetch("/mongo", options)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("mongo", data);
       list.innerHTML = "";
       data.forEach((item) => {
         const li = document.createElement("li");
--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 const DB = require("nedb");
+
+const URI = process.env.ATLAS_URI;
+const connectDB = async () => {
+  await mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("connected to DB");
+};
+
+connectDB();
+app.get("/mongo", (req, res) => {
+  const users = {
+    id: 34256234234,
+    firstName: "john",
+    lastName: "trevolta",
+  };
+  res.send(JSON.stringify(users));
+});
 
 const PORT = process.env.PORT || 8000;
 
@@ -11,7 +33,7 @@ app.listen(PORT, () => console.log("server just ran"));
 app.use(express.static("public"));
 app.use(express.json());
 
-app.post("/api", (req, res) => {
+app.post("/nedb", (req, res) => {
   console.log(req.body);
   database.insert({ name: req.body.name });
   database.find({}, (err, data) => {
